6b3e3b01c15b4c82fc35f384ba27875a9435ef11
Bard-Gaming <christophe@dronne.fr>
2025-08-08 19:09:00 +0200

    feat(api_gen): update config

--- api_gen/objects/config.py
+++ api_gen/objects/config.py
@@ -11,3 +11,20 @@ INDENT_CHAR: Literal[' ', '\t'] = ' '
 
 # Specifies the amount of indent characters used per indent level.
 INDENT_WIDTH: int = 4
+
+
+
+#######################################################################
+##                                                                   ##
+##                               Misc.                               ##
+##                                                                   ##
+#######################################################################
+# Everything here isn't part of the config, so only change
+# if you know what you're doing!
+
+# The config contains global vars, so don't
+# pollute the namespace of the importer by
+# allowing all symbols to get imported (this
+# is a config, so only specific values
+# should get imported anyway)
+__all__ = []
