5043cd787db0b0d0c5710e4488dd189f59cbddb2
Bard-Gaming <christophe@dronne.fr>
2025-08-08 19:13:21 +0200

    feat(api_gen): update objects __init__

--- api_gen/objects/__init__.py
+++ api_gen/objects/__init__.py
@@ -1,1 +1,6 @@
 from api_gen.objects.abc import *
+from api_gen.objects.python_class import *
+from api_gen.objects.python_function import *
+from api_gen.objects.python_method import *
+from api_gen.objects.python_parameter import *
+from api_gen.objects.python_statement import *
